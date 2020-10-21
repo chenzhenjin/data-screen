3dfa04c782cad7b0b90cdfdecbf12437d2660c47
chenzhenjin <971561770@qq.com>
2020-10-21 14:53:23 +0800

    纠正之前错误使用事件绑定和解绑闭包函数

--- src/assets/js/directive.js
+++ src/assets/js/directive.js
@@ -2,29 +2,28 @@
  * @Author: chenzhenjin
  * @email: BrotherStudy@163.com
  * @Date: 2020-06-12 17:11:33
- * @LastEditTime: 2020-09-16 17:36:33
+ * @LastEditTime: 2020-10-21 14:51:52
  * @Descripttion: 模块描述
  */
-var globalBinding = {} //解决匿名函数不能取消dom事件
-var globalNode = {} //解决匿名函数不能取消dom事件
-const debounce = function() {
-  // return function(){
-  if (globalBinding.value.resizeTime) {
-    clearTimeout(globalBinding.value.resizeTime)
+const debounce = function(fn, awaitTime = 500) {
+  return function() {
+    if (fn.timer) {
+      clearTimeout(fn.timer)
+    }
+    fn.timer = setTimeout(() => {
+      console.log('debounce')
+      fn.timer = null
+      fn()
+    }, awaitTime)
   }
-  globalBinding.value.resizeTime = setTimeout(() => {
-    globalBinding.value.resizeTime = null
-    globalBinding.value()
-  }, globalNode.data.attrs.resizeTime)
-  // }
 }
+let bindDebounce = function() {}
 const install = function(Vue) {
   Vue.directive('bindResize', {
     bind: function(el, binding, vnode) {
       //元素节点 绑定的参数 bind.value
-      globalBinding = binding
-      globalNode = vnode
-      window.addEventListener('resize', debounce)
+      bindDebounce = debounce(binding.value, vnode.data.attrs.resizeTime)
+      window.addEventListener('resize', bindDebounce)
       // window.addEventListener("resize", binding.value);
       console.log('bindResize', el, binding, vnode) //vnode.content可以拿到chart
     }
@@ -32,7 +31,7 @@ const install = function(Vue) {
   Vue.directive('unBindResize', {
     unbind: function(el, binding, vnode) {
       //元素节点 绑定的参数 bind.value
-      window.removeEventListener('resize', debounce)
+      window.removeEventListener('resize', bindDebounce)
       // window.removeEventListener("resize", binding.value);
       console.log('unBindResize', el, binding, vnode)
     }
